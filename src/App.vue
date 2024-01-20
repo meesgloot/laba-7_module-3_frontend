0a723a92b33a92805b63fd1bc05dcb425ab01824
meesgloot <shmakova.nyura@bk.ru>
2024-01-20 14:56:17 +0300

    add listMovies.json

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="center">
+    <div v-for="(item, index) in data" :key="item.id" @click="isActive[index] = !isActive[index]" class="cards" :class="{ active: isActive[index] }">
+      <p class="main">{{ item.name }}</p>
+      <button @click="isActive[index] = !isActive[index]">Hidden</button>
+      <img :src="item.img" alt="Movie">
+      <p class="text">{{ item.text }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import json from './components/listMovies.json';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      data: json,
+      isActive: new Array(json.length).fill(false),
+      
+    };
+  },
+ 
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  button{
+    border-radius: 6px;
+    background-color: aqua;
+    width: 100px;
+    height: 60px;
+    margin-bottom: 15px;
+  }
+  .cards.active {
+    width: 400px; 
+    height: auto;
+  }
+  .text {
+    visibility: hidden;
+    height: 0;
+  
+  }
+  .cards.active .text {
+    visibility: visible;
+    opacity: 1;
+    height: auto;
+  }
+  .main {
+    font-size: x-large;
+  }
+  #center {
+    text-align: center;
+    min-height: 320px;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .cards {
+    background-color: black;
+    color: aliceblue;
+    border-radius: 4px;
+    width: 400px;
+    height: 320px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 10px 20px;
+    text-align: center;
+    box-shadow: 10px 5px 5px aqua;
+  }
+  img{
+    width: auto;
+    height: 200px; 
+    text-align: center;
+  }
+  body{
+    background: url(https://catherineasquithgallery.com/uploads/posts/2023-01/1674292577_catherineasquithgallery-com-p-serie-kirpichiki-fon-foto-186.jpg) ;
+    -webkit-background-size: cover;
+    background-size: cover;
+  }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,49 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+  <div id="center"> 
+    <div v-for="(item, index) in data" :key="item.id" @click="isActive[index] = !isActive[index]" class="cards">
+     <p class="main">{{ item.name }}</p> 
+     <img :src=item.img> 
+     <p :class="{active: isActive[index]}" >{{ item.text }}</p> 
+    </div>
   </div>
-</template>
-
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+</template> 
+<script> 
+import json from './components/listMovies.json'
+export default { 
+  data(){
+    return{ 
+      data: json,
+      isActive: new Array(json.length).fill(true)
+    } 
+  },
 }
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+</script> 
+<style> 
+.cards.active{
+  width: 400px; 
+  height: auto; 
+} 
+.active{ 
+  visibility: hidden;
+ } 
+.main{ 
+  font-size: x-large; 
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#center{ 
+  text-align: center; 
+  min-height:320px;
+  display: flex; 
+} 
+.cards{ 
+  background-color: black; 
+  color:aliceblue;
+  border-radius: 4px;
+  width: 400px;
+  height: 320px; 
+  display: inline-block; 
+  vertical-align: middle;
+  margin: 10px 20px;
+  text-align: center;
+  box-shadow: 10px 5px 5px aqua; 
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
